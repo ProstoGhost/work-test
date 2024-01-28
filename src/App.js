ec1fe480234f97bc07aa843e8128bf7b4a464436
ProstoGhost <tam_1910@bk.ru>
2024-01-29 01:37:09 +0500

    finished project version 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
-import "./App.css";
+
 
 const App = () => {
   return (
-    <div className="container">
+    <div>
       <SearchBar/>
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,78 +1,80 @@
 import React, { useState } from "react";
-import "./SearchBar.css"; // Import the CSS file for styling
+import "./SearchBar.css";
+ // Import the CSS file for styling
 
 const SearchBar = () => {
 
-  const countries = [
+  const [value, setValue] = useState('');
+  const [integer, setInteger] = useState('');
+  const [count, setCount] = useState('')
 
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent:"Asia" },
-  
-  ];
-  const [inputText, setInputText] = useState("");
-  let inputHandler = (e) => {
-    //понижаем регистр текста
-    var lowerCase = e.target.value;
-    setInputText(lowerCase);
-  };
-
-  const filteredData = countries.filter((el) => {
-    //если у нас пустая строка поиска то просто выводим всё что есть
-    if (inputText.length === 0) {
-        return el;
-    }
-    //если пользхователь ввел хотя бы один символ то наичнаем поиск
-    if(inputText.length >= 1) {
-      //поиск по стране или по части света
-      if (el.name.includes(inputText)) {
-        return el.name.includes(inputText);
+  const handleChange = (event) => {
+    fetch(`https://swapi.dev/api/people/?search=${event.target.value}`).then(d => d.json()).then(people => {
+      //береам апиху и работаем с ее результатом
+      let answer = people.results[0]
+      //для упрощения берем первый результат
+      setCount(people.count)
+      if(people.count > 1){
+        setInteger(false)
+      }
+      if (people.count === 1) {
+        setValue(answer)
+        setInteger(true)
       }
       else{
-        return el.continent.includes(inputText);
+        setInteger(false)
       }
+      //тут проверяем на условие что у нас нашёлся именно один челик
+      //и кидаем пару хуков в след функцию
+    })
+  }
+  function CreateData(data, checktable){
+    if(checktable){
+      //проверяем что чел только один через checktable и если
+      //один то возвращяем карточку с его данными
+      return <div className="card">
+      <ul>
+        <li>Name: {data.name}</li>
+        <li>Gender: {data.gender}</li>
+        <li>Birth: {data.birth_year}</li>
+        <li>Mass: {data.mass}</li>
+        <li>Height: {data.height}</li>
+      </ul>
+      </div>
     }
-  })
+    if(!checktable){
+      if(count === 82){
+        return <p>Please Enter Name</p>
+      }
+      if(count > 1){
+        return <p>too many answer</p>
+      }
+      if(count === 0){
+        return <p>No match found</p>
+      }
+      //если находит несколько делаем разные ответы либо нет никого либо их много
+      //если пустая строка то пишем что нужно ввести имя всего персов 82 поэтому проверяем условие таким образом
+    }
+  }
+
+  
 
 
 
   return (
-    <div className="search-bar">
-      <p>Search employee</p>
-      <input
-        className="bar-test"
-        type="text"
-        onChange={inputHandler}
+    <div className="main">
+      <h1>Star Wars character search</h1>
+      <div className="search-string">
+        <input
+          className="search-bar"
+          type="text"
+          onChange={handleChange}
         // тут у нас идет текст который мы ввели он уходит в функцию
-        placeholder="Поиск..."
-      />
-      <ul>
-        {filteredData.map((item) =>(
-          <li key={item.name}>{item.continent}: {item.name}</li>
-          //простой геник таблички
-        ))}
-      </ul>
+          placeholder="Поиск..."
+        />
+        {CreateData(value, integer)}
+        {/* тут выводим табличку с результатом */}
+      </div>
     </div>
   );
 };
